a83fef388e2d6657c2a77f0fb8f1b62dfed43aac
Saifuddinafi <afi.saifudin7@gmail.com>
2024-01-28 00:09:44 +0700

    latihan redux

---
+++ src/Redux/pembahasan/Counter.jsx
@@ -0,0 +1,18 @@
+import { useDispatch, useSelector } from "react-redux"
+import { decrementWitchCheckingAction, increment } from "../../app/Counter/actions";
+
+
+const Counter = () => {
+    let {count} = useSelector(state => state.counter);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <button onClick={() => dispatch(decrementWitchCheckingAction(1))}>-</button>
+            {' '} <span>{count}</span> {' '}
+            <button onClick={() => dispatch(increment(1))}>+</button>
+        </div>
+    )
+}
+
+export default Counter;
